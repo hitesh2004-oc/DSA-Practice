ffe0c5a2619c6a82f581f2dbe0d7e5cbde7d095c
Hitesh Malviya <hiteshmalviya955@gmail.com>
2026-07-13 08:57:04 +0530

    Practice

---
+++ P1.java
@@ -0,0 +1,32 @@
+package com.hitesh;
+
+public class P1 {
+	public static void main(String[] args) {
+//		String s = "JavaProgramming";
+//        int a = s.length();
+//        int b = a - 3;
+//		System.out.println(s.substring(b,a));
+//		System.out.println(s.substring(0,3));
+//		String s1 = "123";
+//		int a1 = Integer.parseInt(s1);
+//	    System.out.println(a1);
+	    
+//	    String s2 = "leetcode";
+//	    System.out.println();
+//	    System.out.println(s2.indexOf('t'));
+//	    int a = s2.charAt(3);
+//	    System.out.println(s2.indexOf(a));
+	    
+	    
+//	   int a = 11;
+//	   long b =  11133313342222L;
+//	   short  c = (short) -40000;
+//	 System.out.println(c);
+	 
+	   
+	    
+	}
+
+	
+
+}
