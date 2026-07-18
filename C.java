d28c33b4e0cce4053d868c07b93d841b0fbc1e45
Hitesh Malviya <hiteshmalviya955@gmail.com>
2026-07-18 09:05:06 +0530

    Practice

---
+++ C.java
@@ -0,0 +1,14 @@
+package com.hitesh;
+
+public class C {
+	public static void main(String[] args) {
+		String s = "Java";
+		s = s + "Programming"; // 2 object Create in SCP
+		System.out.println(s);
+		
+		String s3 = "Java" + "Programming"; // 3 object Create in SCP
+		System.out.println(s3);
+		
+	}
+
+}
