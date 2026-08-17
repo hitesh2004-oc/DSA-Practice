5a021f2df8534eb5d51a7f5bd63ced8e2ab5487d
Hitesh Malviya <hiteshmalviya955@gmail.com>
2026-08-17 07:50:23 +0530

    Recursion Practice
    Added a comment to clarify the purpose of the sum method.

--- P3.java
+++ P3.java
@@ -2,11 +2,11 @@ package Recursive;
 
 public class P3 {
 	  public static int sum(int n){
+		  // Resurrection Method 
 		  if(n==0) return 0;
 		  int add = sum(n/10);
 		  return n%10 + add;
-	    }
-	  
+	  }
 	public static void main(String[] args) {
 		System.out.println(sum(598));
 	}
