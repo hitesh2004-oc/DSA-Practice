1e65c37db088d0bcb6fdeea6639dd2dad8d236d0
Hitesh Malviya <hiteshmalviya955@gmail.com>
2026-08-18 08:29:41 +0530

    Practice Anagram
    Added comments to each step for better understanding.

--- Anagram.java
+++ Anagram.java
@@ -4,19 +4,24 @@ import java.util.Arrays;
 //...Check Anagram or Not...//
 public class Anagram {
 	public static void main(String[] args) {
+		// Step 1
 		String str1 = "School Master";
 		String str2 = "The Classroom";
 
+		// Step 2
 		str1 = str1.replace(" ", "").toLowerCase();
 		str2 = str2.replace(" ", "").toLowerCase();
 
+		// Step 3 Convert
 		char arr1[] = str1.toCharArray();
 		char arr2[] = str2.toCharArray();
 
+		// Step 4 Sort 
 		Arrays.sort(arr1);
 		Arrays.sort(arr2);
-
-		if (Arrays.equals(arr1, arr2)) {
+		
+		// Step 5 Check
+		if(Arrays.equals(arr1, arr2)) {
 			System.out.println("Anagram");
 		} else
 			System.out.println("Not Anagram");
